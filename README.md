9ead488bbec0e725f4b0170df2428c732d6d922e
Gabriel Galli <g7.galli96@gmail.com>
2016-10-08 16:27:11 -0300

    README update, from <file> import *

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # cfg_analyzer
 Context-free grammar analyzer
 
-- Terminals must be enclosed with '"' (the terminals '"' and '\' must be escaped, e.g.: "\"");
-- '|' is used to separate productions and '&' is &epsilon;
+- Terminals must be enclosed with `"` (double quotes), e.g.: `"if"`
+- The terminals `"` (double quotes) and `\` (backslash) must be escaped, e.g.: `"\""` and `"\\"`
+- `|` (pipe) is used to separate productions and `""` (empty string) means &epsilon;
--- cfg.py
+++ cfg.py
@@ -1,13 +1,13 @@
 #!/bin/python3.5
 import sys
-import Analyzer as an
-import NDFA
+from Analyzer import *
+from NDFA import *
 
 def main(argv):
     # The with statement allows objects like files to be used in a way
     # that ensures they are always cleaned up promptly and correctly.
     with open(argv[1], 'r') as f:
-        a = an.Analyzer()
+        a = Analyzer()
         a.readgr(f)
         print("Special states:")
         print(a.asterisk)
@@ -15,10 +15,10 @@ def main(argv):
         print(a.ignore)
         print("\nGrammar:")
         a.printgr()
-        # print("---------------------")
-        # b = NDFA.NDFA(a.grammar, a.asterisk)
-        # print("---------------------")
-        # b.printndfa()
+        print("---------------------")
+        b = NDFA(a.grammar, a.asterisk)
+        print("---------------------")
+        b.printndfa()
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
