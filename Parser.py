2054467d532c46f9b7aebf88def3e54925dd72d5
Gabriel Galli <g7.galli96@gmail.com>
2016-11-04 12:06:52 -0200

    Lex almost done; added ' ' to our grammar

--- Parser.py
+++ Parser.py
@@ -7,7 +7,44 @@ class Parser():
         self.dfa = dfa
         self.output = []
         self.symbols = {}
-        self.separators = {" ", "(", ")", "*", "/", "%", "^", "?", ":", "<", "=", ">", "[", "]", "{", "}", ".", ",", ";", "'", "\"", "\n"}
+        self.separators = {chr(1), " ", "(", ")", "*", "/", "%", "^", "?", ":", "<", "=", ">", "[", "]", "{", "}", ".", ",", ";", "'", "\"", "\n"}
+
+    def spaces(self, line):
+        CHAR = 0
+        CHAR2 = 1
+        QUOTE = 2
+        ESC = 3
+        state = CHAR
+        newline = ""
+        for char in line:
+            if state == CHAR:
+                if char == ' ':
+                    continue
+                    # newline += chr(1)
+                    # state = CHAR2
+                elif char == '"':
+                    newline += char
+                    state = QUOTE
+                else:
+                    newline += char
+            # elif state == CHAR2:
+            #     if char == ' ':
+            #         continue
+            #     elif char == '"':
+            #         newline += char
+            #         state = QUOTE
+            #     else:
+            #         newline += char
+            elif state == QUOTE:
+                newline += char
+                if char == '\\':
+                    state = ESC
+                elif char == '"':
+                    state = CHAR
+            elif state == ESC:
+                newline += char
+                state = QUOTE
+        return newline
 
     def parse(self, file):
         i = 1
@@ -16,22 +53,45 @@ class Parser():
         current_state = 0
         line = file.readline()
         while line != "":
-            print(line)
-            for c in line:
-                char = ord(c)
-                print("char {}: {}".format(c, char))
+            line = self.spaces(line)
+            print("-------------------------------------\nline: {}".format(line))
+            caret = 0
+            wasFinal = False
+            while caret < len(line):
+                char = line[caret]
+                c = ord(char)
+                print("current_state: {}\nchar '{}': {}".format(current_state, char if char != '\n' else "\\n", c))
                 token_end += 1
-                if c in self.separators:
-                    if current_state not in self.dfa.finals and char not in self.dfa.table[current_state]:
+                if char in self.separators:
+                    print("in separators")
+                    if current_state not in self.dfa.finals and c not in self.dfa.table[current_state]:
                         raise LexicalError(INVALID_TOKEN, i, "'{}'".format(line[token_start:token_end]))
-                    if char not in self.dfa.table[current_state]:
+                    if c not in self.dfa.table[current_state] or wasFinal:
                         self.output.append(current_state)
-                        current_state = 0
-                        token_start = token_end + 1
+                        token_start = token_end
+                        if c in self.dfa.table[0]:
+                            print("isFinal\ntransition from {} to {} through '{}'".format(current_state, self.dfa.table[0][c], char))
+                            current_state = self.dfa.table[0][c]
+                            wasFinal = True
+                            caret += 1
+                        else:
+                            print("reset")
+                            current_state = 0
+                            if char == '\n':
+                                caret += 1
+                        print("output: {}\n\n".format(self.output))
                         continue
-                if char in self.dfa.table[current_state]:
-                    current_state = self.dfa.table[current_state][char]
+                if c in self.dfa.table[current_state]:
+                    print("transition from {} to {} through '{}'".format(current_state, self.dfa.table[current_state][c], char))
+                    current_state = self.dfa.table[current_state][c]
+                elif wasFinal == True and c in self.dfa.table[0]:
+                    print("wasFinal")
+                    self.output.append(current_state)
+                    current_state = self.dfa.table[0][c]
                 else:
-                    print("'{}' not found on state {}".format(c, current_state))
+                    raise LexicalError(INVALID_TOKEN, i, "'{}' not mapped on state {}".format(char, current_state))
+                wasFinal = False
+                print("output: {}\n\n".format(self.output))
+                caret += 1
             i += 1
             line = file.readline()
